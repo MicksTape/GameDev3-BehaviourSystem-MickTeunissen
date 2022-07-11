b507e67638c1e45115ad7cb5057188b6a9153739
Mick Teunissen <37049425+MicksTape@users.noreply.github.com>
2022-07-11 08:40:21 +0200

    Opdracht 1 en 2

---
+++ Boids-Opdr1/Assets/Scripts/Coherence.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cohesion : MonoBehaviour
+{
+    public GameObject coherencePointVisual;
+
+    private BoidManager manager;
+    private Vector2 sumOfAllBoidPositions;
+
+    private void Start()
+    {
+        manager = GetComponent<BoidManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sumOfAllBoidPositions = new Vector2();
+        foreach (Boid boid in manager.boids)
+        {
+            sumOfAllBoidPositions += boid.position;
+        }
+    }
+
+
+    public Vector2 GetCoherence(Boid currentBoid){
+
+        Vector2 centerOfMassOfBoidsEceptCurrentBoid = (sumOfAllBoidPositions - currentBoid.position) / (manager.boids.Length - 1);
+        coherencePointVisual.transform.position = centerOfMassOfBoidsEceptCurrentBoid;
+        return (centerOfMassOfBoidsEceptCurrentBoid - currentBoid.position).normalized;
+
+    }
+}
